c8509c2ca134e8d3c637e9e2a1185a26a9ec6c8f
Eric Cramer <emcramer@stanford.edu>
2016-11-24 00:50:17 -0800

    added plots of mlm model

--- kaggle.R
+++ kaggle.R
@@ -8,4 +8,8 @@ cor(train)
 
 # Simple multiple linear regression'
 train.dat <- train[-1, ]
-mlm.fit <- lm(value ~., data = train.dat)
+mlm.fit <- lm(Value ~., data = train.dat)
+par(mfrow=c(2,2))
+plot(mlm.fit) # Not sure what to make of the residuals distribution, thoughts?
+par(mfrow=c(1,1))
+
